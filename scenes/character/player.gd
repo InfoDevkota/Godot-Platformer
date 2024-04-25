1c7a4d488127c0635f32798becc3708b9ea8743e
Sagar Devkota <info@devkotasagar.com.np>
2024-04-25 17:33:00 +0545

    Install Android Build Add Android Export Add actions to Input Map Add TouchScreenButton Hide TouchScreenButon on non Android device

--- scenes/character/player.gd
+++ scenes/character/player.gd
@@ -32,14 +32,14 @@ func _physics_process(delta):
 		
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	
 	if !is_hit:
-		var direction = Input.get_axis("ui_left", "ui_right")
+		var direction = Input.get_axis("left", "right")
 		if direction:
 			velocity.x = direction * SPEED
 			if direction -1:
