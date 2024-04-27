0209959002e9df7041b4d2898f5d5c7592e566f7
Sagar Devkota <info@devkotasagar.com.np>
2024-04-27 15:55:10 +0545

    Add Explosion effect at last for space ship     Add Sound as well     prepare Scene two, used level 0 for now, need to be replaced with exact one     remove (set volume to zero) for voice over for now

--- scenes/cutscenes/opening_cut_scene.gd
+++ scenes/cutscenes/opening_cut_scene.gd
@@ -11,3 +11,9 @@ func _ready():
 func _process(delta):
 	parallax_layer.motion_offset.x += 20 * delta
 	pass
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "space":
+		get_tree().change_scene_to_file("res://scenes/cutscenes/opening_cut_scene_scene_2.tscn")
+	pass # Replace with function body.
