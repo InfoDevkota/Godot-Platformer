22084c2f33315cf4edd4067466e6bd7672edcde6
Sagar Devkota <info@devkotasagar.com.np>
2024-04-27 21:17:16 +0545

    A Basic Initial Open Scene completed

---
+++ scenes/levels/level_opening_cutscene.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	# for dev
+	#$AnimationPlayer.play("start")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func start_the_animation():
+	$AnimationPlayer.play("start")
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "start":
+		get_tree().change_scene_to_file("res://scenes/levels/level_0.tscn")
+	pass # Replace with function body.
